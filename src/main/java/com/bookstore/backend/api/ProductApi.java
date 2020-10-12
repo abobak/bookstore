fddbfc204e5fbd06511791fe6acb6d8627c8db7d
Andrzej Bobak <a.bobak@interia.pl>
2020-10-12 20:26:57 +0200

    Create product api contract

--- src/main/java/com/bookstore/backend/api/ProductApi.java
+++ src/main/java/com/bookstore/backend/api/ProductApi.java
@@ -1,4 +1,26 @@
 package com.bookstore.backend.api;
 
+import com.bookstore.backend.dto.ProductDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+import java.util.List;
+
+@Api(value = "product")
 public interface ProductApi {
+
+    @ApiOperation(value = "Get single product")
+    ProductDto getProduct(Long id);
+
+    @ApiOperation(value = "Get all products")
+    List<ProductDto> getProducts();
+
+    @ApiOperation(value = "Create product")
+    ProductDto createProduct(ProductDto dto);
+
+    @ApiOperation(value = "Update product")
+    ProductDto updateProduct(ProductDto dto);
+
+    @ApiOperation(value = "Delete product")
+    void deleteProduct(Long id);
 }
