fe45e26911ddbe26fc884d4d39d45c32719d917c
Andrzej Bobak <a.bobak@interia.pl>
2020-10-13 23:17:45 +0200

    Add API documentation and next steps

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# bookstore
+# bookstore app
+Swagger API documentation located under http://localhost:8080/swagger-ui/
+
+Next steps:
+- security: OAuth, client applications need to authenticate against trusted authority, their token will allow access to backend resources
+- scalability: implement a dispatcher service and a gatekeeper service (in case resources are limited, e.g. code will be used to maintain finite resources)
  
--- src/main/java/com/bookstore/backend/api/PurchaseApi.java
+++ src/main/java/com/bookstore/backend/api/PurchaseApi.java
@@ -2,13 +2,18 @@ package com.bookstore.backend.api;
 
 import com.bookstore.backend.dto.NewPurchaseDto;
 import com.bookstore.backend.dto.PurchaseDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 import java.util.List;
 
+@Api(value = "purchase")
 public interface PurchaseApi {
 
+    @ApiOperation(value = "Place an order")
     PurchaseDto placeOrder(NewPurchaseDto newPurchaseDto);
 
+    @ApiOperation(value = "List all orders create between provided dates in yyyy-MM-dd format")
     List<PurchaseDto> listOrders(String from, String to);
 
 }
